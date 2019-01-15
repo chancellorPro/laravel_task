758abedfdf8dbc3cfb303f281c7f947c71b40bcf
floky <pavel@zolotve.pp.ua>
2019-01-15 21:19:12 +0200

    some minor fixes

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -14,10 +14,13 @@
                             <div class="form-group{{ $errors->has('avatar') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">Avatar</label>
                                 @if (file_exists(public_path() . '/avatars/' . $user->avatar))
-                                    <img class="pull-right" src="/avatars/{{ $user->avatar }}" width="100">
+                                    <img id="avatar" class="col-md-6" src="/avatars/{{ $user->avatar }}">
                                 @endif
+                            </div>
+                            <div class="form-group{{ $errors->has('avatar') ? ' has-error' : '' }}">
+                                <label class="col-md-4"></label>
                                 <div class="col-md-6">
-                                    <input type="file" class="form-control-file" name="avatar" id="avatarFile"
+                                    <input style="display: none" type="file" class="form-control-file" name="avatar" id="avatarFile"
                                            aria-describedby="fileHelp" onchange="this.form.submit()">
                                     <small id="fileHelp" class="form-text text-muted">Please upload a valid image file.
                                         Size of image should not be more than 2MB.
@@ -78,6 +81,10 @@
     <script type="text/javascript">
         jQuery(function ($) {
             $("#phone").mask("+(999) 999-9999");
+
+            $("#avatar").on("click", function () {
+                $('input[type="file"]').click();
+            });
         });
     </script>
 @endsection
